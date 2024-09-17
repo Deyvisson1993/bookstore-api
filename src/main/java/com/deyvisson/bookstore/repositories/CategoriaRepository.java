1a83ddb2f909c19d00495e1d9eb8ebe42b9f9c4d
Deyvisson Mendes <deyvisson.mendes@condominiolpbv.local>
2024-09-17 11:22:58 -0300

    instacia da base de dados H2

---
+++ src/main/java/com/deyvisson/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.deyvisson.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.deyvisson.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
